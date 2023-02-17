c2ac4653d1c3f39376fa7139e059874851d5e839
David <dryan10101@gmail.com>
2023-02-17 00:37:46 -0800

    adding original work done before repo was created

---
+++ APICallMaker.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class APICallMaker {
+    String url;
+    
+    public APICallMaker(String url) {
+        this.url = url;
+    }
+
+    public void newVehicleEntry(String vehicleData) {
+        //create http client
+        HttpClient client = HttpClient.newHttpClient();
+
+        try {
+
+            //create request
+            var request = HttpRequest.newBuilder()
+            .PUT(HttpRequest.BodyPublishers.ofString(vehicleData))
+            .header("Content-Type", "application/json")
+            .uri(URI.create(this.url + "/vehicle"))
+            .build();
+
+            //send request
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            //print response
+            System.out.println("Status code: " + response.statusCode());                            
+            System.out.println("Headers: " + response.headers().allValues("content-type"));
+            System.out.println("Body: " + response.body());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ POS.java
@@ -0,0 +1,116 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class POS {
+    //currently this system takes user input via a Scanner
+    //later on it may be updated to have a gui, but that is not the main concern as of right now
+
+    public Vehicle getVehicleDataFromDB(Vehicle v) {
+
+        return null;
+    }
+
+    public void sendEmployeeDataToDB(Employee p) {
+
+    }
+
+    public Employee getEmployeeDataFromDB(Employee p) {
+
+        return null;
+    }
+
+    public static Vehicle manuallyCreateVehicle() {
+        //vcs = Vehicle Creation Scanner
+        Scanner vcs = new Scanner(System.in);
+        Map<String, String> map = new HashMap<>();
+
+        System.out.println("Creating new vehicle.  To skip a step, type 'skip' into the entry");
+        System.out.println("Enter vehicle license plate:");
+        String lp = vcs.nextLine();
+        if (!lp.equals("skip")) {
+            map.put("licensePlate", lp);
+        }
+        System.out.println("Enter license plate state or country");
+        String s = vcs.nextLine();
+        if (!s.equals("skip")) {
+            map.put("licensePlateOrigin", s);
+        }
+        System.out.println("Enter vehicle make:");
+        String mk = vcs.nextLine();
+        if (!mk.equals("skip")) {
+            map.put("make", mk);
+        }
+        System.out.println("Enter vehicle color:");
+        String c = vcs.nextLine();
+        if (!c.equals("skip")) {
+            map.put("color", c);
+        }
+        System.out.println("Enter guest's last name:");
+        String ln = vcs.nextLine();
+        if (!ln.equals("skip")) {
+            map.put("guestLastName", ln);
+        }
+        System.out.println("Enter guest's first name:");
+        String fn = vcs.nextLine();
+        if (!fn.equals("skip")) {
+            map.put("guestFirstName", fn);
+        }
+        System.out.println("Create vehicle? (Y/N)");
+        boolean finalize = vcs.nextLine().equals("Y") ? true : false;
+        vcs.close();
+        if (finalize) {
+            return new Vehicle(map);
+        } else {
+            return null;
+        }
+    }
+    
+    public static void main(String[] args) {
+        boolean exit = false;
+        Scanner scanner = new Scanner(System.in);
+        APICallMaker apiCaller = new APICallMaker("https://lwxsjt5kvg.execute-api.us-west-2.amazonaws.com/");
+
+        System.out.println("Welcome to the automated Valet system!");
+
+        while (!exit) {
+            System.out.println("\n");
+            System.out.println("1) New Vehicle Parked");
+            System.out.println("2) Edit Existing Vehicle Info");
+            System.out.println("3) Vehicle Exiting");
+            //more options to come
+
+            //take chosen option input
+            String chosenOption = scanner.nextLine();
+            switch (chosenOption) {
+                case "1":
+                    //enter new vehicle info
+                    Vehicle v = manuallyCreateVehicle();
+                    v.sendToDB(apiCaller);
+                    break;
+                
+                case "2":
+                    //edit existing vehicle info
+                    break;
+                
+                case "3":
+                    //exiting vehicle process
+                    break;
+                
+                default:
+                    System.out.println("Unknown input value");
+
+            }
+
+
+            /* System.out.println("Exit? (Y/N)");
+            String exitLine = scanner.nextLine();
+            if (exitLine.equals("Y")) {
+                exit = true;
+            } */
+            exit = true;
+        }
+
+        scanner.close();
+    }
+}
