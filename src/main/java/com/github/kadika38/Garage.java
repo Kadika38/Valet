b11df08ad4f9f477d45e864a684afcf50e4f7f72
David <dryan10101@gmail.com>
2023-04-17 15:26:16 -0700

    added Garage.java and 2 functions within it: hasSpot() and update()

---
+++ src/main/java/com/github/kadika38/Garage.java
@@ -0,0 +1,42 @@
+package com.github.kadika38;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Garage {
+    Map<Spot, Vehicle> garage;
+    ArrayList<Spot> spotList;
+
+    Garage(ArrayList<Spot> spotList) {
+        this.spotList = spotList;
+        for (Spot spot : this.spotList) {
+            this.garage.put(spot, null);
+        }
+    }
+
+    public boolean hasSpot(String spotName) {
+        for (Spot spot : this.spotList) {
+            if (spotName.equals(spot.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void update(Vehicle v) {
+        String spotName = v.getLocation();
+        String vid = v.getVid();
+        // find if vehicle was already in a spot in the garage, and remove it from that spot if so
+        for (Spot spot : this.spotList) {
+            if (this.garage.get(spot).getVid().equals(vid)) {
+                this.garage.put(spot, null);
+            }
+        }
+        // put vehicle in current spot
+        for (Spot spot : this.spotList) {
+            if (spotName.equals(spot.getName())) {
+                this.garage.put(spot, v);
+            }
+        }
+    }
+}
